decd17882ed008eda31d8dbdf36d3b5cba4824a8
dadagama <dadagama@2184f5ef-0d61-596e-e0fe-5714389f195d>
2010-05-16 11:17:44 +0000

    se adapta nueva botonera para el funcionamiento

--- trunk/modulo_contabilidad/contabilidad.php
+++ trunk/modulo_contabilidad/contabilidad.php
@@ -27,38 +27,38 @@ switch($_REQUEST['accion'])
 /**********************************
 ******* ACCIONES GENERICAS  *******
 ***********************************/
-	
+
 	case "mostrarPanel":
 		require_once("../modulo_panel/fm_panel.php");
 		break;
-	
+/*	
 	case "actualizarListaClientes":
 		echo $objetoContabilidad->actualizarListaClientes(trim($_REQUEST['info_cliente']));
 		break;
-		
+*/		
 	case "actualizarCampoColor":
 		$prefijo = $_REQUEST['prefijo'];
 		echo $objetoContabilidad->actualizarCampoColor($_REQUEST[$prefijo.'_id'],$_REQUEST[$prefijo.'_color_fila'], $prefijo);
 		break;
-		
+/*		
 	case "actualizarEstadoDeuda":
-		//se retorna el prefijo de la zona de actualizacion
+		// se retorna el prefijo de la zona de actualizacion
 		echo $objetoContabilidad->actualizarEstadoDeuda($_REQUEST['prefijo'], $_REQUEST['id_fila'], $_REQUEST['estado']);
 		break;
 	
 	case "eliminarFila":
-		//se retorna el id de la fila eliminada
+		// se retorna el id de la fila eliminada
 		echo $objetoContabilidad->eliminarFila($_REQUEST['id'], $_REQUEST['prefijo']);
 		break;
 		
 	case "actualizarObservacion":
 		$objetoContabilidad->actualizarObservacion($_REQUEST['prefijo'], $_REQUEST['id_fila'], $_REQUEST['observacion']);
 		break;
-		
+	*/	
 /**********************************
 ******* ACCIONES PRODUCTOS  *******
 ***********************************/
-
+/*
 	case "agregarFilaProducto":
 		echo $objetoContabilidad->agregarFilaProducto($_REQUEST['vep_cli_id'], 
 																									$_REQUEST['vep_pro_id'],
@@ -69,26 +69,24 @@ switch($_REQUEST['accion'])
 	case "actualizarHistoricoProductos":
 		$objetoContabilidad->actualizarHistoricoProductos();
 		break;
-		
+		*/
 /**********************************
 ******* ACCIONES SERVICIOS  *******
 ***********************************/
 
 	case "agregarFilaServicio":
-		$objetoContabilidad->agregarFilaServicio($_REQUEST['ves_ser_id'],
-																							$_REQUEST['ves_hora'],
-																							$_REQUEST['ves_minuto'],
-																							$_REQUEST['ves_meridiano'],
-																							$_REQUEST['ves_duracion'],
-																							$_REQUEST['ves_total'],
-																							$_REQUEST['ves_cli_id'],
-																							$_REQUEST['ves_fecha']);
+		echo $objetoContabilidad->agregarFilaServicio($_REQUEST['ves_ser_id'],
+																										$_REQUEST['ves_hora'],
+																										$_REQUEST['ves_minuto'],
+																										$_REQUEST['ves_meridiano'],
+																										$_REQUEST['ves_duracion'],
+																										$_REQUEST['ves_fecha']);
 		break;
 	
-	case "actualizarHistoricoServicios":
-		$objetoContabilidad->actualizarHistoricoServicios();
+	case "actualizarHistorialVentas":
+		$objetoContabilidad->actualizarHistorialVentas();
 		break;
-		
+	/*
 	case "actualizarTotalServicio":
 		echo $objetoContabilidad->actualizarTotalServicio($_REQUEST['servicio'],$_REQUEST['duracion']);
 		break;
@@ -96,13 +94,14 @@ switch($_REQUEST['accion'])
 	case "actualizarDuracionYTotalServicio":
 		echo $objetoContabilidad->actualizarDuracionYTotalServicio($_REQUEST['id'],$_REQUEST['duracion']);
 		break;
-
+*/
 /**********************************
 ******* ACCIONES TIMERS     *******
 ***********************************/
-
+/*
 	case "obtenerInfoTimers":
 		echo json_encode($objetoContabilidad->obtenerInfoTimers());
 		break;
+*/
 }
 ?>

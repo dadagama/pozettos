318fe11c24637340bf1a96a2a37d265fba1132cf
dadagama <dadagama@2184f5ef-0d61-596e-e0fe-5714389f195d>
2010-07-13 04:11:41 +0000

    se agrega tabla y logica para plata que sale

--- trunk/modulo_contabilidad/contabilidad.php
+++ trunk/modulo_contabilidad/contabilidad.php
@@ -44,6 +44,10 @@ switch($_REQUEST['accion'])
 		echo $objetoContabilidad->actualizarSaldoTitan($_REQUEST['sit_saldo'], $_REQUEST['sit_fecha']);
 		break;
 	
+	case "actualizarSaldoEgresos":
+		echo $objetoContabilidad->actualizarSaldoEgresos($_REQUEST['egd_saldo'], $_REQUEST['egd_fecha']);
+		break;
+	
 	case "actualizarDeudaCliente":
 			$objetoContabilidad->actualizarValor($_REQUEST['hiv_id'],$_REQUEST['valor_nuevo'],"hiv_cli_id");
 			//$valor_nuevo tiene el id del nuevo cliente y puedo consultar la deuda
